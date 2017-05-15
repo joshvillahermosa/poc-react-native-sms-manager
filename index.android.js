411431fa12c83be007c21c44701df9814cb3738f
Joshua J. Villahermosa <joshvillahermosa@gmail.com>
2017-05-15 19:51:54 -0400

    chore(app): Implement native-base library

--- index.android.js
+++ index.android.js
@@ -8,46 +8,37 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
-  View
+  Text
 } from 'react-native';
+
+import { 
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  Body,
+  Icon
+} from 'native-base'
 
 export default class poc extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          POC React Native SMS Manager
-        </Text>
-        <Text style={styles.instructions}>
-          This is a POC (Proof of concept) for Android SMS Reader. This will read Contacts Lists, Send SMS, and Read SMS.
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Container>
+        <Header>
+          <Body>
+            <Title>POC React Native SMS Manager</Title>
+          </Body>
+        </Header>
+        
+        <Content padder>
+          <Text>
+            This is a POC (Proof of concept) for Android SMS Reader. This will read Contacts Lists, Send SMS, and Read SMS.
+          </Text>
+        </Content>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('poc', () => poc);
